0f50768149345e8abcf4ad49f94d6afbde752290
Thomas Levine <_@thomaslevine.com>
2015-03-14 22:18:25 +0000

    chords

--- generate-data/intervals.R
+++ generate-data/intervals.R
@@ -1,17 +1,24 @@
 intervals <- list(P1=0, P4=5, P5=7, P8=12,
+                  M4=5, m4=4, M5=7, m5=6,
                   M2=2, M3=4, M6=9, M7=11,
                   m2=1, m3=3, m6=8, m7=10)
 attach(intervals)
 
-chords <- list(M = cumsum(c(P1, M3, P5)),
-               m = cumsum(c(P1, m3, P5)),
-               M7 = cumsum(c(P1, M3, P5, M7)),
-               m7 = cumsum(c(P1, M3, P5, m7)))
+chords <- list(M = c(P1, M3, P5),
+               m = c(P1, m3, P5),
+               dim = c(P1, m3, m5),
+               aug = c(P1, M3, M5))
+chords$M7 <- c(chords$M, M7)
+chords$m7 <- c(chords$m, m7)
+chords$'M7+' = c(chords$aug, M7)
+chords$'m7+' = c(chords$aug, m7)
 
 scales <- list(natural.minor = c(P1, m2, m3, P4, P5, m6, m7),
                major = c(P1, M2, M3, P4, P5, M6, M7))
 
 detach(intervals)
 
-for (x in ls())
-  save(intervals, paste0('data/', x, '.RData'))
+for (x in c('intervals', 'chords', 'scales')) {
+  y <- get(x)
+  save(y, file = paste0('data/', x, '.RData'))
+}
