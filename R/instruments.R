bff648af2a7afb257a20123a6a77d2585cd55fe9
Thomas Levine <_@thomaslevine.com>
2015-03-15 05:00:52 +0000

    -

--- R/instruments.R
+++ R/instruments.R
@@ -6,10 +6,16 @@ fade.linear <- curry(fade.polynomial, 1)
 fade.quadratic <- curry(fade.polynomial, 2)
 fade.cubic <- curry(fade.polynomial, 3)
 
+fade.elbow <- function(duration, elbow.position = 1/8, elbow.amplitude = 1/5) {
+  elbow.position.absolute <- round(elbow.position * duration)
+  c(seq(1, elbow.amplitude, length.out = elbow.position.absolute),
+    seq(elbow.amplitude, 0, length.out = duration - elbow.position.absolute))
+}
+
 instrument <- function(tone, fade)
   function(frequency, duration)
     tone(frequency, duration) * fade(duration)
 
 harpsicord <- instrument(curry(overtones, 1:13, sawtooth), fade.sqrt)
 xylophone <- instrument(curry(overtones, c(1, 2, 4, 8), sine), fade.cubic)
-
+clave <- instrument(square, fade.elbow)
