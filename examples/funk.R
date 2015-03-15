c3bbc7a83b8cabb5b05e38c43352721163c9bd36
Thomas Levine <_@thomaslevine.com>
2015-03-15 02:36:38 +0000

    -

--- examples/funk.R
+++ examples/funk.R
@@ -4,3 +4,10 @@
 # https://en.wikipedia.org/wiki/Ostinato#Vamp
 # https://en.wikipedia.org/wiki/Clave_%28rhythm%29
 # https://en.wikipedia.org/wiki/Guajeo
+
+# Eight beats
+clave.starts <- c(2, 3, 5, 6.5, 8)
+clave.durations <- c(1, 1, 1.5, 0.5, 1)
+
+arpeggio.starts <- c(1, 2, 3, 3.5, 4.5, 5.5, 6.5, 7, 8)
+arpeggio.durations <- c(1, 1, 0.5, 1, 1, 0.5, 1, 1)
