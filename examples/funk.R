3b63157a34b296920c41a4214259db52069dfb3b
Thomas Levine <_@thomaslevine.com>
2015-03-15 05:38:10 +0000

    -

--- examples/funk.R
+++ examples/funk.R
@@ -27,10 +27,10 @@ track <- function(frequencies, starts, durations = 0.5,
 
 onbeat.offbeat.23 <- track(110,
                            c(2, 3, 5, 6.5, 8),
-                           durations = c(1, 1, 1.5, 0.5, 1),
+                           durations = c(1, 1, 1.5, 0.5, 1) / 4,
                            instrument = clave)
 
-sax <- track(P.n(scales$mixolydian[c(1, 3, 5)][c(3, 1, 3, 3, 2, 2, 1)], a = 12),
+sax <- track(P.n(scales$mixolydian[c(1, 3, 5)][c(3, 1, 3, 3, 2, 2, 1)], a = 24),
              c(1, 3, 5.5, 6.5, 7, 7.5, 8),
              durations = c(1, 1, 0.5, 0.5, 0.5, 0.5, 0.5),
              instrument = saxaphone)
@@ -40,4 +40,4 @@ arpeggios <- track(P.n(scales$mixolydian[c(1, 3, 5, 7, 3, 7, 5, 2, 3)], a = 0),
                    c(1, 1, 0.5, 1, 1, 0.5, 1, 1, 1),
                    instrument = sinesynth)
 
-play(rep(onbeat.offbeat.23 + 2 * sax + arpeggios, 4))
+play(rep(0.5 * onbeat.offbeat.23 + 2 * sax + arpeggios, 4))
