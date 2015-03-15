01626428553d81dc6f0ab8dab791c8a050818715
Thomas Levine <_@thomaslevine.com>
2015-03-15 05:09:17 +0000

    -

--- examples/funk.R
+++ examples/funk.R
@@ -10,10 +10,15 @@ source('generate-data.R')
 # https://en.wikipedia.org/wiki/Clave_%28rhythm%29
 # https://en.wikipedia.org/wiki/Guajeo
 
-phrase <- function(bpm = 120, beats = 8)
-
-eight.beats <- function(bpm, starts, durations, pitches, instrument, fade) {
-  n.samples <- SECONDS * 8 * bpm / 60
+track <- function(pitches, starts, durations = 0.5,
+                  instrument = sinesynth,
+                  bpm = 120, beats = 8, sampling.rate = SECONDS)
+  a <- length(pitches)
+  b <- length(starts)
+  c <- length(durations)
+  if (a %% b != 0 || b %% c != 0 || c %% a != 0 || a %% c != 0 || c %% b != 0 || b %% a != 0)
+    stop('Pitches, starts, and durations must have the same length in clock arithmetic.')
+  n.samples <- sampling.rate * 8 * bpm / 60
   eight.beat.add.note(n.samples)
 }  
 eight.beat.add.note <- function(n.samples, start, duration, pitch, instrument, fade) {
@@ -28,18 +33,10 @@ eight.beat.add.note <- function(n.samples, start, duration, pitch, instrument, f
 clave.starts <- c(2, 3, 5, 6.5, 8)
 clave.durations <- c(1, 1, 1.5, 0.5, 1)
 clave.pitches <- scales$mixolydian[c(1, 3, 5)]
-clave.instrument <- curry(square, 220)
-clave.fade <- function(duration) {
-  elbow <- round(duration / 8)
-  elbow.amplitude <- 0.2
-  c(seq(1, elbow.amplitude, length.out = elbow),
-    seq(elbow.amplitude, 0, length.out = duration - elbow))
-}
 
 sax.starts <- c(1, 3, 5.5, 6.5, 7, 7.5, 8)
 sax.durations <- c(1, 1, 0.5, 0.5, 0.5, 0.5, 0.5)
 sax.pitches <- scales$mixolydian[c(1, 3, 5)][c(3, 1, 3, 3, 2, 2, 1)]
-sax.instrument <- curry(overtones, seq(1, 2^4, 2), square)
 
 arpeggio.starts <- c(1, 2, 3, 3.5, 4.5, 5.5, 6.5, 7, 8)
 arpeggio.durations <- c(1, 1, 0.5, 1, 1, 0.5, 1, 1)
