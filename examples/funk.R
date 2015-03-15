b1a988061240547a2e95e586b6c376cc6d80976a
Thomas Levine <_@thomaslevine.com>
2015-03-15 02:28:20 +0000

    -

---
+++ examples/funk.R
@@ -0,0 +1,6 @@
+# https://en.wikipedia.org/wiki/Funk
+# https://en.wikipedia.org/wiki/Dorian_mode
+# https://en.wikipedia.org/wiki/Mixolydian_mode
+# https://en.wikipedia.org/wiki/Ostinato#Vamp
+# https://en.wikipedia.org/wiki/Clave_%28rhythm%29
+# https://en.wikipedia.org/wiki/Guajeo
