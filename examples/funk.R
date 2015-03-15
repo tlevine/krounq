682ae8e42cef3873a05815af0495488d96c5d20e
Thomas Levine <_@thomaslevine.com>
2015-03-15 05:29:22 +0000

    -

--- examples/funk.R
+++ examples/funk.R
@@ -15,7 +15,7 @@ track <- function(frequencies, starts, durations = 0.5,
                   bpm = 240, beats = 8, sampling.rate = SECOND) {
   notes <- data.frame(frequency = frequencies, start = starts, duration = durations)
 
-  n.samples <- sampling.rate * 8 * bpm / (4 * 60)
+  n.samples <- 8 * sampling.rate * 60 / bpm
   beat <- seq(1, 9, length.out = n.samples)
   waveform <- rep(0, n.samples)
   for (i in 1:nrow(notes)) {
@@ -33,7 +33,7 @@ onbeat.offbeat.23 <- track(220,
 sax <- track(P.n(scales$mixolydian[c(1, 3, 5)][c(3, 1, 3, 3, 2, 2, 1)], a = 12),
              c(1, 3, 5.5, 6.5, 7, 7.5, 8),
              durations = c(1, 1, 0.5, 0.5, 0.5, 0.5, 0.5),
-             instrument = saxaphone)
+             instrument = sinesynth)
 
 arpeggio.starts <- c(1, 2, 3, 3.5, 4.5, 5.5, 6.5, 7, 8)
 arpeggio.durations <- c(1, 1, 0.5, 1, 1, 0.5, 1, 1)
