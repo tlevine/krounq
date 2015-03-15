506092a43946d4705d02adc0b878fdc75110fdd2
Thomas Levine <_@thomaslevine.com>
2015-03-15 04:55:19 +0000

    -

--- R/instruments.R
+++ R/instruments.R
@@ -9,3 +9,7 @@ fade.cubic <- curry(fade.polynomial, 3)
 instrument <- function(tone, fade)
   function(frequency, duration)
     tone(frequency, duration) * fade(duration)
+
+harpsicord <- instrument(curry(overtones, 1:13, sawtooth), fade.sqrt)
+xylophone <- instrument(curry(overtones, c(1, 2, 4, 8), sine), fade.cubic)
+
