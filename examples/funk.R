15a897694413054c45c1bb2d5395f6df8bbb262a
Thomas Levine <_@thomaslevine.com>
2015-03-15 02:48:14 +0000

    -

--- examples/funk.R
+++ examples/funk.R
@@ -8,6 +8,11 @@
 # Eight beats
 clave.starts <- c(2, 3, 5, 6.5, 8)
 clave.durations <- c(1, 1, 1.5, 0.5, 1)
+clave.pitches <- scales$mixolydian[c(1, 3, 5)]
+
+sax.starts <- c(1, 3, 5.5, 6.5, 7, 7.5, 8)
+sax.durations <- c(1, 1, 0.5, 0.5, 0.5, 0.5, 0.5)
+sax.pitches <- scales$mixolydian[c(1, 3, 5)][c(3, 1, 3, 3, 2, 2, 1)
 
 arpeggio.starts <- c(1, 2, 3, 3.5, 4.5, 5.5, 6.5, 7, 8)
 arpeggio.durations <- c(1, 1, 0.5, 1, 1, 0.5, 1, 1)
