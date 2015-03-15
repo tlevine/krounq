1aa9e87cf87b50e8750022a96f6f2111c354b7c2
Thomas Levine <_@thomaslevine.com>
2015-03-15 15:17:03 +0000

    -

--- examples/dub.R
+++ examples/dub.R
@@ -4,12 +4,15 @@ devtools::load_all()
 
 source('generate-data.R')
 
-key <- P.n(0, a = 0)
 attach(intervals)
+f <- function(x) P.n(x, a = 12)
 
-downbeat <- track(key - P8 + c(P1, P5), c(1, 5), 1)
+downbeat <- sequence(f(P5), c(1, 5), 2)
+triplets <- sequence(f(P1), seq(1, 5, length.out = 10)[-10], 1/3, beats = 4)
+
 #' onbeat.offbeat.23 <- track(110,
 #'                            c(2, 3, 5, 6.5, 8),
 #'                            durations = c(1, 1, 1.5, 0.5, 1) / 4)
 
-play(downbeat)
+play(rep(triplets, 2))
+#play(rep(downbeat + triplets, 2))
