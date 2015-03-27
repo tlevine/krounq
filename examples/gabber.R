88ce5c70ffbd9cbdbb263ba9eced5c0e2ddc2202
Thomas Levine <_@thomaslevine.com>
2015-03-27 22:37:25 +0000

    -

--- examples/gabber.R
+++
@@ -1,7 +0,0 @@
-# https://en.wikipedia.org/wiki/Gabber
-# https://en.wikipedia.org/wiki/Hardcore_%28electronic_dance_music_genre%29
-# https://en.wikipedia.org/wiki/Happy_hardcore
-# Thunderdome 96
-#
-# Or, plan B:
-# https://en.wikipedia.org/wiki/Music_of_the_Netherlands#Folk
