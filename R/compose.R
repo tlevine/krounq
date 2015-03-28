b43682da417937addd02b082f99c332c878fbcd2
Thomas Levine <_@thomaslevine.com>
2015-03-28 11:07:44 +0000

    -

--- R/compose.R
+++ R/compose.R
@@ -36,18 +36,24 @@ invert.chord <- function(chord, full.inversion) {
 #' onbeat.offbeat.23 <- track(110,
 #'                            c(2, 3, 5, 6.5, 8),
 #'                            durations = c(1, 1, 1.5, 0.5, 1) / 4)
-sequence <- function(frequencies, starts, durations = 0.5,
+sequence <- function(frequencies = 440, starts = NULL, durations = 0.5,
                      instrument = sinesynth,
                      tempo = 240, beats = 8, sampling.rate = SECOND) {
+  if (is.null(starts)) {
+    if (length(durations) != 1)
+      stop('If starts is NULL, durations must have length 1.')
+    starts <- seq(1, beats + 1, durations)
+    starts <- starts[-length(starts)]
+  }
   notes <- data.frame(frequency = frequencies, start = starts, duration = durations)
 
   n.samples <- beats * sampling.rate * 60 / tempo
-  beat <- seq(1, 9, length.out = n.samples)
+  beat <- seq(1, beats, length.out = n.samples)
   waveform <- rep(0, n.samples)
   for (i in 1:nrow(notes)) {
     selector <- beat >= notes[i,'start'] & beat < (notes[i,'start'] + notes[i,'duration'])
     waveform[selector] <- instrument(notes[i,'frequency'], sum(selector))
   }
-  waveform
+  waveform[1:n.samples]
 }
 
--- examples/gabber.R
+++ examples/gabber.R
@@ -1,6 +1,11 @@
 library(devtools)
 unloadNamespace('devtools')
+library(ddr)
+data(roland)
+unloadNamespace('ddr')
 devtools::load_all()
+data(kick)
+source('generate-data.R')
 
 # Aesthetics
 # * Tilt
@@ -8,6 +13,10 @@ devtools::load_all()
 # * X
 # * Y
 # * 
+
+norm <- function(x) {
+  x / max(abs(x))
+}
 
 # x <- clip(square(220, SECOND/2) + 0.2 * white.noise(SECOND/2))
 scratch <- function(freq, duration) {
@@ -18,23 +27,31 @@ scratch <- function(freq, duration) {
   c(base.left, base.right)
 }
 
-kick.instrument <- function(., duration) {
-  data(kick)
-  if (duration < length(kick))
-    stop(paste('Duration must be at least', length(kick)))
-  c(kick, silence(duration - length(kick)))
+sample.instrument <- function(the.sample) {
+  function(., duration) {
+    if (duration < length(the.sample))
+      # stop(paste('Duration must be at least', length(the.sample)))
+      the.sample[1:duration]
+    else
+      c(the.sample, silence(duration - length(kick)))
+  }
 }
 
+kick.instrument <- sample.instrument(kick)
+
+snare <- sample.instrument(norm(roland$SD0@left))
+hihat <- sample.instrument(norm(roland$HHO@left))
+
 # quarter, quarter, triplets | quarter, two eighths, four eighths
-b <- sequence(300, c(1, 2, 3, 3 + 2/3, 4 + 1/3,
-                     1, 2, 2.5, 3, 3.5, 4, 4.5),
+a <- sequence(0, c(1, 2, 3, 3 + 2/3, 4 + 1/3,
+                   5, 6, 6.5, 7, 7.5, 8, 8.5),
               durations = 1, instrument = kick.instrument,
-              tempo = 205)
+              tempo = 205, beats = 8)
+b <- sequence(durations = .25, instrument = snare,
+              tempo = 205, beats = 8)
+d <- sequence(frequencies = P.n(40 + scales$major[c(1,3,2,1)]),
+              starts = c(1, 5, 9, 13), durations = 4,
+              instrument = sine, tempo = 205, beats = 16)
+play(d)
 
-play(b[-length(b)])
-
-#freq <- P.n(7, a = 0)
-#drums <- sequence(freq, c(1, 2 - 1/8, 3, 4, 4.5),
-#                  c(0.75, 1, 0.75, 0.5, 0.5),
-#                  beats = 4, tempo = 90)
-#play(rep(drums, 4))
+#play(rep(a + b + d, 2))
