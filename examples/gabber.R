194ff60e441ab65fea965a75a5d15533692fdf27
Thomas Levine <_@thomaslevine.com>
2015-03-28 11:09:44 +0000

    -

--- examples/gabber.R
+++ examples/gabber.R
@@ -19,13 +19,16 @@ norm <- function(x) {
 }
 
 # x <- clip(square(220, SECOND/2) + 0.2 * white.noise(SECOND/2))
-scratch <- function(freq, duration) {
+bass.drum <- function(freq, duration) {
   base <- 0.8 * sine(freq, duration) ^ 3 + runif(duration, -.2, .2)
   duration.left <- round(SECOND/8)
   base.left <- base[1:duration.left]
   base.right <- silence(duration - duration.left)
   c(base.left, base.right)
 }
+
+scratch <- function(freq, duration)
+  (0.8 * sine(freq, duration) ^ 3 + runif(duration, -.2, .2)) * fade.linear(duration)
 
 sample.instrument <- function(the.sample) {
   function(., duration) {
@@ -51,7 +54,7 @@ b <- sequence(durations = .25, instrument = snare,
               tempo = 205, beats = 8)
 d <- sequence(frequencies = P.n(40 + scales$major[c(1,3,2,1)]),
               starts = c(1, 5, 9, 13), durations = 4,
-              instrument = sine, tempo = 205, beats = 16)
-play(d)
+              instrument = scratch, tempo = 205, beats = 16)
+#play(d)
 
-#play(rep(a + b + d, 2))
+play(a + b + 3 * d)
